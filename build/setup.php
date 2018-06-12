d9c2d50908c8d9effdff7ad0a119af92b1e627ab
dfantuzzi <davide.fantuzzi@trivago.com>
2018-06-12 10:50:19 +0200

    new setup script

---
+++ build/setup.php
@@ -0,0 +1,18 @@
+<?php
+
+$rootDir = dirname(dirname(__FILE__));
+$appname = basename($rootDir);
+
+$filesToReplace = [
+    $rootDir . '/build/docker-compose.yml',
+    $rootDir . '/.env.example', 
+];
+
+array_map(function($file) use ($appname) {
+    $content = file_get_contents($file);
+    file_put_contents($file, str_replace('{appname}', $appname, $content));
+}, $filesToReplace);
+
+if (!file_exists('.env')) {
+    copy('.env.example', '.env');
+}
